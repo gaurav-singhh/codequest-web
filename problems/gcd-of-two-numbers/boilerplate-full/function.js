b681e7e7ee34e6d910d2b1f5fba704c1574f55fc
GauravSingh-1609 <gauravkr.singh1609@gmail.com>
2025-09-19 16:21:41 +0530

    refactor: encapsulate input handling in an IIFE and update test input format

--- problems/gcd-of-two-numbers/boilerplate-full/function.js
+++ problems/gcd-of-two-numbers/boilerplate-full/function.js
@@ -1,9 +1,12 @@
 
+
 ##USER_CODE_HERE##
 
-const input = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n').join(' ').split(' ');
-const a = parseInt(input.shift());
-  const b = parseInt(input.shift());
-const result = gcd(a, b);
-console.log(result);
+(function() {
+  const _harness_input = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n').join(' ').split(' ');
+  const _harness_a = parseInt(_harness_input.shift());
+  const _harness_b = parseInt(_harness_input.shift());
+  const _harness_result = gcd(_harness_a, _harness_b);
+  console.log(_harness_result);
+})();
     
